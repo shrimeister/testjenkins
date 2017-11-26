8f9914df6de9a39c88d2f0a67764fc1e891f9099
Shripathi Balaji <shrimeister@gmail.com>
2017-11-26 14:22:08 +0800

    test jenkins

---
+++ src/main/java/TestClass.java
@@ -0,0 +1,71 @@
+/* IMPORTANT: Multiple classes and nested static classes are supported */
+
+
+//imports for BufferedReader
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+
+class TestClass {
+    public static void main(String args[] ) throws Exception {
+        Scanner br = new Scanner(System.in);
+        String tests = br.nextLine();
+
+        double numTests = Double.parseDouble(tests);
+
+        if( numTests >= 1 && numTests <= 3) {
+            String cellSize = br.nextLine();
+
+            String[] cellSizeSplit = cellSize.split(" ");
+
+            if (cellSizeSplit.length == 3) {
+                int x = Integer.parseInt(cellSizeSplit[0]);
+                int y = Integer.parseInt(cellSizeSplit[1]);
+                double numHappyCells = Double.parseDouble(cellSizeSplit[2]);
+
+                if (x >= 1 && y >= 1 && numHappyCells <= Math.pow(10, 5)) {
+                    int[][] twoDArray = new int[x][y];
+
+                    while (numHappyCells != 0) {
+                        String kLine = br.nextLine();
+
+                        String[] kLineSplit = kLine.split(" ");
+
+                        if (kLineSplit.length == 3) {
+                            int a = Integer.parseInt(kLineSplit[0]);
+                            int b = Integer.parseInt(kLineSplit[1]);
+                            int happyValue = Integer.parseInt(kLineSplit[2]);
+
+                            if(a <= x && b <= y && happyValue >= 1 && happyValue <= Math.pow(10,5))
+                                if (a - 1 <= x - 1 && b - 1 <= y - 1) {
+                                    twoDArray[a - 1][b - 1] = happyValue;
+                                }
+                        }
+                        numHappyCells--;
+                    }
+
+                    int happyValue = 0;
+                    int temp = 0;
+                    for (int i = 0; i <= x - 1; i++) {
+                        for (int j = temp; j <= y - 1; j++) {
+
+                            happyValue += twoDArray[i][j];
+                            if (j + 1 <= y - 1) {
+                                happyValue += twoDArray[i][j + 1];
+                                System.out.println(happyValue);
+                                temp = j + 1;
+                            }
+                            temp = 0;
+                            break;
+                        }
+                    }
+                    if(happyValue > 1000000007)
+                        happyValue = happyValue % 1000000007;
+
+                    System.out.println(happyValue);
+                }
+            }
+        }
+    }
+}
